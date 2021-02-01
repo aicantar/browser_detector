b059d23826b442a2a0239fd34f03863d8e641ba8
Phil Chekhov <me@aicantar.com>
2021-02-01 21:02:43 +0600

    Export the Engine class and Engines enum

--- lib/browser_detector.dart
+++ lib/browser_detector.dart
@@ -3,6 +3,9 @@ library browser_detector;
 export 'package:browser_detector/src/browser_detector.dart'
     show BrowserDetector;
 
-export 'package:browser_detector/src/enums.dart' show Browsers, Platforms;
+export 'package:browser_detector/src/enums.dart'
+    show Browsers, Platforms, Engines;
+
 export 'package:browser_detector/src/browser.dart' show Browser;
 export 'package:browser_detector/src/platform.dart' show Platform;
+export 'package:browser_detector/src/engine.dart' show Engine;
