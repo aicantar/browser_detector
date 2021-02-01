258743dccf937118aa10989ff35b8ddc11fa839c
Phil Chekhov <me@aicantar.com>
2021-02-01 20:33:58 +0600

    Move BrowserDetector into src, user agent providers to separate dir

--- lib/browser_detector.dart
+++ lib/browser_detector.dart
@@ -1,61 +1,8 @@
 library browser_detector;
 
-export 'package:browser_detector/src/enums.dart' show Browsers;
+export 'package:browser_detector/src/browser_detector.dart'
+    show BrowserDetector;
+
+export 'package:browser_detector/src/enums.dart' show Browsers, Platforms;
 export 'package:browser_detector/src/browser.dart' show Browser;
-
-import 'package:browser_detector/src/browser.dart';
-
-import 'package:browser_detector/src/fake_native_user_agent.dart'
-    if (dart.library.html) 'package:browser_detector/src/native_user_agent.dart';
-import 'package:browser_detector/src/platform.dart';
-
-/// Detects browser, engine and platform the app is running on based on the user
-/// agent string.
-class BrowserDetector {
-  /// Known browsers.
-  Iterable<Browser> _browsers = [
-    Chrome(),
-    Firefox(),
-    Safari(),
-    Edge(),
-  ];
-
-  /// Known platofrms.
-  Iterable<Platform> _platforms = [
-    Windows(),
-    MacOS(),
-    Linux(),
-    IOS(),
-    IPadOS(),
-    Android(),
-  ];
-
-  /// Private [Browser] instance.
-  Browser _browser;
-
-  /// Private [Platform] instance.
-  Platform _platform;
-
-  /// Detected [Browser].
-  Browser get browser => _browser;
-
-  /// Detected [Platform].
-  Platform get platform => _platform;
-
-  /// Construct a BrowserDetector. If the [userAgent] is passed, the detector
-  /// will not retrieve the value from the environment and use the parameter
-  /// instead.
-  BrowserDetector([String userAgent]) {
-    final actualUserAgent = userAgent ?? nativeUserAgent;
-
-    _browser = _browsers.firstWhere(
-      (browser) => browser.matcherCallback(actualUserAgent),
-      orElse: () => Browser.unknown(),
-    );
-
-    _platform = _platforms.firstWhere(
-      (platform) => platform.matcherCallback(actualUserAgent),
-      orElse: () => Platform.unknown(),
-    );
-  }
-}
+export 'package:browser_detector/src/platform.dart' show Platform;
---
+++ lib/src/browser_detector.dart
@@ -0,0 +1,56 @@
+import 'package:browser_detector/src/environment/fake_native_user_agent.dart'
+    if (dart.library.html) 'package:browser_detector/src/environment/native_user_agent.dart';
+
+import 'browser.dart';
+import 'platform.dart';
+
+/// Detects browser, engine and platform the app is running on based on the user
+/// agent string.
+class BrowserDetector {
+  /// Known browsers.
+  Iterable<Browser> _browsers = [
+    Chrome(),
+    Firefox(),
+    Safari(),
+    Edge(),
+  ];
+
+  /// Known platofrms.
+  Iterable<Platform> _platforms = [
+    Windows(),
+    MacOS(),
+    Linux(),
+    IOS(),
+    IPadOS(),
+    Android(),
+  ];
+
+  /// Private [Browser] instance.
+  Browser _browser;
+
+  /// Private [Platform] instance.
+  Platform _platform;
+
+  /// Detected [Browser].
+  Browser get browser => _browser;
+
+  /// Detected [Platform].
+  Platform get platform => _platform;
+
+  /// Construct a BrowserDetector. If the [userAgent] is passed, the detector
+  /// will not retrieve the value from the environment and use the parameter
+  /// instead.
+  BrowserDetector([String userAgent]) {
+    final actualUserAgent = userAgent ?? nativeUserAgent;
+
+    _browser = _browsers.firstWhere(
+      (browser) => browser.matcherCallback(actualUserAgent),
+      orElse: () => Browser.unknown(),
+    );
+
+    _platform = _platforms.firstWhere(
+      (platform) => platform.matcherCallback(actualUserAgent),
+      orElse: () => Platform.unknown(),
+    );
+  }
+}
